1264787a6be6c35ec2285547a02a47548f5aeb38
María Mercedes Retolaza Reyna <ret16339@uvg.edu.gt>
2018-05-01 13:51:30 -0600

    insert function

--- src/tokenizationClass.py
+++ src/tokenizationClass.py
@@ -105,3 +105,56 @@ class tokenizationClass(sqlListener):
         if (check):
             (dbFile.renameFS(table_name_old, table_name_new))
     # !ALTER TABLE
+
+     # INSERT
+    def enterInsert_stmt(self, ctx: sqlParser.Insert_stmtContext):
+
+        tableName = self.getTokenValue(ctx.table_name())
+        tableData = eval(dbFile.readTableFS(tableName, "data"))
+
+        # table structure
+        tableStructure = eval(dbFile.readTableFS(tableName, "structure"))
+        # input col structure
+        targetCols = [self.getTokenValue(col) for col in ctx.column_name()]
+
+        newData = []
+
+        values = [self.getTokenValue(value) for value in ctx.expr()]
+        colNames = [col[0] for col in tableStructure]
+        colTypes = [col[1] for col in tableStructure]
+
+        if len(values) > len(tableStructure):
+            raise ValueError("LA PETICIÓN DE INSERTAR TIENE MÁS VALORES QUE LA LONGITUD DE LA MESA")
+
+        # specific insert stmt
+
+        if len(targetCols):
+            newData = [""] * len(tableStructure)
+            for targetCol in targetCols:
+                if targetCol in colNames:
+                    colIndex = targetCols.index(targetCol)
+                    valueIndex = colNames.index(targetCol)
+                    if (not len(values) > valueIndex):
+                        newData[colIndex] = 'NULL'
+                    else:
+                        newData[colIndex] = dataCM.matchData(colTypes[colIndex], values[valueIndex])
+                else:
+                    raise ValueError("LA COLUMNA " + targetCol + " NO EXISTE EN LA TABLA " + tableName)
+
+        # regular insert stmt
+
+        else:
+            index = 0
+            for value in ctx.expr():
+                newData.append(dataCM.matchData(tableStructure[index][1], self.getTokenValue(value)))
+                index = index + 1
+
+        if len(newData):
+            if None in newData:
+                raise ValueError("AL MOMENTO DE INSERTAR LA CONSULTA TIENE TIPOS INCONSISTENTES")
+            tableData.append(tuple(newData))
+            dbFile.insertTableFS(tableName, str(tableData))
+            print("INSERT A " + tableName + " EXITOSO")
+        else:
+            dataCM.raiseError(False, "NO SE PUEDE INSERTAR EN UNA TUPLA VACÍA")
+    # !INSERT
